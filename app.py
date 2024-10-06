b07ba1d859877a76d85ad264175fe22e3051f986
Srgr0 <66754887+Srgr0@users.noreply.github.com>
2024-10-06 17:21:05 +0900

    Add download button

--- app.py
+++ app.py
@@ -1,31 +1,48 @@
 import gradio as gr
 import os
+import shutil
 from generate import create_multiple_large_images
 
 BASE_PATH = "./generated_images/"
+ZIP_PATH = "./generated_images.zip"
 
 def generate_images(size_mb, num_images):
     if not os.path.exists(BASE_PATH):
         os.makedirs(BASE_PATH)
 
+    for filename in os.listdir(BASE_PATH):
+        file_path = os.path.join(BASE_PATH, filename)
+        if os.path.isfile(file_path):
+            os.unlink(file_path)
+
     created_image_paths = create_multiple_large_images(BASE_PATH, size_mb, num_images)
 
-    return created_image_paths
+    if os.path.exists(ZIP_PATH):
+        os.remove(ZIP_PATH)
+    shutil.make_archive("generated_images", 'zip', BASE_PATH)
+
+    return created_image_paths, ZIP_PATH
 
 def create_ui():
     with gr.Blocks() as demo:
-        gr.Markdown("# test-image-generator")
-        size_input = gr.Slider(minimum=1, maximum=100, label="Target size of the images in MB")
-        num_images_input = gr.Slider(minimum=1, maximum=10, step=1, label="Number of images to generate")
+        gr.Markdown("# Test Image Generator")
+
+        with gr.Row():
+            size_input = gr.Slider(minimum=1, maximum=100, label="Target size of the images in MB")
+            num_images_input = gr.Slider(minimum=1, maximum=100, step=1, label="Number of images to generate")
+
         generate_button = gr.Button("Generate")
 
-        output = gr.Gallery(label="Generated images", columns=3, height='400px')
+        output_gallery = gr.Gallery(label="Generated Images", columns=3, height='400px').style(grid=[3])
+        download_button = gr.File(label="Download All Images as ZIP")
+
 
         generate_button.click(
             fn=generate_images,
             inputs=[size_input, num_images_input],
-            outputs=output
+            outputs=[output_gallery, download_button]
         )
+
     return demo
 
 if __name__ == "__main__":
